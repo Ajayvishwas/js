45c69b5be6db2561ae3a8975b98127241d1aade0
Ajay Vishwas <iajayvishwas@gmail.com>
2023-09-13 22:57:44 +0530

    CALLBACK

--- newcallback.js
+++ newcallback.js
@@ -1,15 +1,21 @@
-function call(m,callback)
+function call(a,callback)
 {
  console.log("this is callback");
     return callback;
-}
+};
 
 function add(a1, b1)
 {
-    return a1+ b1;
-}
+    return a1+b1;
+};
 function sub(a2, b2)
 {
-    return a2-b2;
+  return a2-b2;
 
-}
+};
+
+let res = call("this is call back", add(1,3));
+let ressub = call("this is call back", sub(1,3));
+
+console.log(res);
+console.log(ressub);
